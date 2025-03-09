62a561b8e48246520b0cbb5f168e29a4dad975b1
JayanithyaMadhushani <121443632+Jayanithyaa@users.noreply.github.com>
2025-03-10 01:22:43 +0530

    03/09

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -8,7 +8,7 @@ import CheckIcon from "@/assets/icons/check-circle.svg";
 import { Education } from "@/sections/Education";
 import { ExperienceRoadmap } from "./ExperienceRoadmap";
 import { CertificationSection } from "./CertificationSection";
-import  VolunteeringSection  from "./volunteeringAndLeadership";
+import VolunteeringSection from "./volunteeringAndLeadership";
 
 export const Introduction = () => {
   const [visibleSection, setVisibleSection] = useState<string | null>(null);
@@ -24,25 +24,31 @@ export const Introduction = () => {
         {/* Left Section - Text Content */}
         <div className="max-w-lg text-center md:text-left">
           <h2 className="font-mono text-2xl  bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">
-            Hey there! It's me,
+            Hey there! It&apos;s me,
           </h2>
-          <h1 className="text-4xl mt-4 md:text-5xl text-white">Jayanithya Madhushani.</h1>
+
+          <h1 className="text-4xl mt-4 md:text-5xl text-white">
+            Jayanithya Madhushani.
+          </h1>
           <div className="border-t border-white/15 mt-8 py-2"></div>
           <p className="mt-4 text-white/80 leading-relaxed">
             I am a final-year IT undergraduate from Sri Lanka, passionate about
             building innovative and scalable solutions. With a strong focus on
-            problem-solving, creativity, and continuous learning, I strive to push
-            the boundaries of technology.
+            problem-solving, creativity, and continuous learning, I strive to
+            push the boundaries of technology.
           </p>
           <ul className="space-y-2 text-white/50 mt-8">
             <li className="flex items-center gap-2">
-              <CheckIcon className="size-5 md:size-6 text-gray-400" /> Frontend Development
+              <CheckIcon className="size-5 md:size-6 text-gray-400" /> Frontend
+              Development
             </li>
             <li className="flex items-center gap-2">
-              <CheckIcon className="size-5 md:size-6 text-gray-400" /> Backend Development
+              <CheckIcon className="size-5 md:size-6 text-gray-400" /> Backend
+              Development
             </li>
             <li className="flex items-center gap-2">
-              <CheckIcon className="size-5 md:size-6 text-gray-400" /> UI/UX Design
+              <CheckIcon className="size-5 md:size-6 text-gray-400" /> UI/UX
+              Design
             </li>
           </ul>
         </div>
@@ -63,7 +69,10 @@ export const Introduction = () => {
           { title: "Experiences", component: <ExperienceRoadmap /> },
           { title: "Education", component: <Education /> },
           { title: "Certificate", component: <CertificationSection /> },
-          { title: "Volunteering & Leadership", component: <VolunteeringSection /> },
+          {
+            title: "Volunteering & Leadership",
+            component: <VolunteeringSection />,
+          },
         ].map(({ title }) => (
           <button
             key={title}
@@ -74,7 +83,9 @@ export const Introduction = () => {
             {/* Smooth appearing underline */}
             <div
               className={`absolute left-0 bottom-0 w-0 h-0.5 bg-gradient-to-r from-emerald-300 to-sky-400 transition-all duration-500 ease-in-out ${
-                visibleSection === title ? "w-full opacity-100" : "group-hover:w-full group-hover:opacity-100"
+                visibleSection === title
+                  ? "w-full opacity-100"
+                  : "group-hover:w-full group-hover:opacity-100"
               }`}
             ></div>
           </button>
@@ -92,13 +103,17 @@ export const Introduction = () => {
             transition={{ duration: 0.4, ease: "easeInOut" }} // Smooth transition
             className="mt-6 w-full max-w-5xl p-6 bg-white/5 text-gray-300 rounded-lg"
           >
-            {[
-              { title: "Experiences", component: <ExperienceRoadmap /> },
-              { title: "Education", component: <Education /> },
-              { title: "Certificate", component: <CertificationSection /> },
-              { title: "Volunteering & Leadership", component: <VolunteeringSection /> },
-            ]
-              .find((item) => item.title === visibleSection)?.component}
+            {
+              [
+                { title: "Experiences", component: <ExperienceRoadmap /> },
+                { title: "Education", component: <Education /> },
+                { title: "Certificate", component: <CertificationSection /> },
+                {
+                  title: "Volunteering & Leadership",
+                  component: <VolunteeringSection />,
+                },
+              ].find((item) => item.title === visibleSection)?.component
+            }
           </motion.div>
         )}
       </AnimatePresence>
