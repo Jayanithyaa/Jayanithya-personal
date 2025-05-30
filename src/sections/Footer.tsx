43eadbc63e9ce842fa9d6ddd865251d638926096
JayanithyaMadhushani <121443632+Jayanithyaa@users.noreply.github.com>
2025-05-31 01:19:11 +0530

    footer compleed
    create clickable footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,5 +1,4 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import { link } from "fs";
 
 const footerLinks = [
   {
@@ -22,22 +21,21 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
-          <div className="text-white/40">
-            &copy; 2025 Jayanithya Madhushani
-          </div>
+          <div className="text-white/40">&copy; 2025 Jayanithya Madhushani</div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
               <a
-              href={link.href} // Correctly set the href attribute
-              key={link.title}
-              className="inline-flex items-center gap-1.5"
-              target="_blank" // Open links in a new tab
-            >
-                <ArrowUpRightIcon className="size-4" />
+                key={link.title}
+                href={link.href}
+                className="inline-flex items-center gap-1.5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ArrowUpRightIcon className="size-4 pointer-events-none" />
                 <span className="font-semibold">{link.title}</span>
               </a>
             ))}
